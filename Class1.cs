694a70a4717dfcdc826a0d20232f66352b2dfbb8
ADMINS <ADMINS@DESKTOP-QFR4487>
2021-01-24 16:10:39 +0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6_CoffeeShop
+{
+    class Class1
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment6_CoffeeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
