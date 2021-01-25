b4e659c83791bc5e16a87867c68055a5b3a3266b
ADMINS <ADMINS@DESKTOP-QFR4487>
2021-01-25 11:15:20 +0700

    hhd

--- Drink.cs
+++ Drink.cs
@@ -6,5 +6,24 @@ namespace Assignment6_CoffeeShop
 {
     class Drink
     {
+        private string name;  //tea coffee juice
+        private int price;   //45      50   30
+
+        public Drink(string name)
+        {
+            this.name = name;
+            if (name == "coffee") price = 50;
+            if (name == "tea") price = 45;
+            if (name == "juice") price = 30;
+          
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public int GetPrice()
+        {
+            return price;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,50 @@ namespace Assignment6_CoffeeShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         
+            Drink[] drinks = new Drink[10];
+
+            drinks[0] = new Drink("coffee", 50);
+
+            drinks[1] = new Drink("tea", 45);
+            drinks[2] = new Drink("juice", 30);
+            drinks[3] = new Drink("juice", 30);
+            int countOfCoffee = 0;
+            int countOfTea = 0;
+            int countOfJuice = 0;
+            int count = 0;
+            int total = 0;
+            for(int i = 0;i < drinks.Length; i++)
+            {
+                if(drinks[i] != null)
+                {
+                    Console.WriteLine(drinks[i].GetName());
+                    count = count + 1;
+                    total = total + drinks[i].GetPrice();
+
+                    if (drinks[i].GetName() == "coffee")
+                    {
+                        countOfCoffee++;
+
+                    }
+                    else if (drinks[i].GetName() == "tea")
+                    {
+                        countOfTea++;
+
+                    }
+                    else if (drinks[i].GetName() == "juice")
+                    {
+                        countOfJuice++;
+
+                    }
+                }
+
+                
+            }
+            Console.WriteLine(countOfCoffee);
+            Console.WriteLine(countOfTea);
+            Console.WriteLine(countOfJuice);
+
         }
     }
 }
